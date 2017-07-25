93f4654b4da3bc027d62d11b9e6c36d04ad6f6fa
Helen Guov <helen@Helens-Mac-mini.fritz.box>
2017-07-25 19:53:07 +1200

    added video click event handler

--- src/components/videoList/video_list.jsx
+++ src/components/videoList/video_list.jsx
@@ -4,21 +4,30 @@ import ReactDOM from 'react-dom';
 import VideoListItem from './video_list_item.jsx';
 
 class VideoList extends React.Component {
-    
+
     constructor(props) {
         super(props);
-        
+
     }
 
     getVideoItems() {
-        if(this.props.videos.length == 0) return "Loading..."; 
-    
+        if (this.props.videos.length == 0) return "Loading...";
+
         let htmlVideoItems = this.props.videos.map(
-            (aVideo) => {         
-                return <VideoListItem key={aVideo.etag} video={aVideo} />
-            }); 
+            (aVideo) => {
+                //assign onVideoClick as a function for the event handler video click 
+                return <VideoListItem
+                    onVideoClick={(vid) => { this.handleVideoClick(vid) }}
+                    key={aVideo.etag}
+                    video={aVideo} />
+            });
         return htmlVideoItems
     }
+
+    handleVideoClick(clickedVideo) {
+        console.log('clicked video ', clickedVideo.snippet.title);
+    }
+
     render() {
         return (
             <ul>{this.getVideoItems()}</ul>
@@ -27,5 +36,5 @@ class VideoList extends React.Component {
     }
 }
 
-    export default VideoList;
+export default VideoList;
 
--- src/components/videoList/video_list_item.jsx
+++ src/components/videoList/video_list_item.jsx
@@ -11,14 +11,17 @@ export default class VideoListItem extends React.Component {
             videoDetails : this.props.video.snippet,
         }
     }
+
     render() {
         return (
-            <li>
+            <li onClick={()=>{this.props.onVideoClick(this.props.video)}}>
                 <div><img src={this.state.thumbnail.url} height={this.state.thumbnail.Height}
                     width={this.state.thumbnail.Width} /></div>
                 <div>{this.state.videoDetails.title}</div> 
             </li>
         );
     }
+
+
 }
 
